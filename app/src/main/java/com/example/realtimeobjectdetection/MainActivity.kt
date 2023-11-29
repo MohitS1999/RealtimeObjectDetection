b166ae4e10fad120160862597ea74825b1839f33
Mohit Singh <mohitsingh@Mohits-MacBook-Air.local>
2023-11-29 09:00:14 +0530

    camera was not working earlier
    Signed-off-by: Mohit Singh <mohitsingh@Mohits-MacBook-Air.local>

--- app/src/main/java/com/example/realtimeobjectdetection/MainActivity.kt
+++ app/src/main/java/com/example/realtimeobjectdetection/MainActivity.kt
@@ -29,43 +29,40 @@ import org.tensorflow.lite.support.image.ops.ResizeOp
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var labels:List<String>
-    var colors = listOf<Int>(
-        Color.BLUE,Color.GREEN,Color.RED,Color.CYAN,Color.GRAY,Color.BLACK,
-        Color.DKGRAY,Color.MAGENTA,Color.YELLOW,Color.RED
-    )
-    private val paint = Paint()
-    private lateinit var imageProcessor: ImageProcessor
-    private lateinit var bitmap: Bitmap
     private lateinit var binding: ActivityMainBinding
-    private lateinit var handler:Handler
-    private lateinit var cameraManager:CameraManager
-    private lateinit var cameraDevice: CameraDevice
+    lateinit var labels:List<String>
+    var colors = listOf<Int>(
+        Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED)
+    val paint = Paint()
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var bitmap:Bitmap
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
+    lateinit var cameraManager: CameraManager
     lateinit var model:ObjectDetection1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        model = ObjectDetection1.newInstance(this)
-        try{
-            labels = FileUtil.loadLabels(this,"labels.txt")
-        }catch (e:IOException){
-            e.printStackTrace()
-        }
+        get_permission()
 
-        getPermission()
-        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300,300,ResizeOp.ResizeMethod.BILINEAR)).build()
-        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        labels = FileUtil.loadLabels(this, "labels.txt")
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build()
+        model = ObjectDetection1.newInstance(this)
         val handlerThread = HandlerThread("videoThread")
         handlerThread.start()
         handler = Handler(handlerThread.looper)
+
+
+
+
         binding.textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
             override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
-                openCamera()
+                open_camera()
             }
-
             override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
-
             }
 
             override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
@@ -74,68 +71,69 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
                 bitmap = binding.textureView.bitmap!!
-
-
-// Creates inputs for reference.
                 var image = TensorImage.fromBitmap(bitmap)
                 image = imageProcessor.process(image)
-// Runs model inference and gets result.
+
                 val outputs = model.process(image)
                 val locations = outputs.locationsAsTensorBuffer.floatArray
                 val classes = outputs.classesAsTensorBuffer.floatArray
                 val scores = outputs.scoresAsTensorBuffer.floatArray
                 val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer.floatArray
 
-                var mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888,true)
-                val canvas = Canvas(mutableBitmap)
+                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
 
-
-                val h = mutableBitmap.height
-                val w = mutableBitmap.width
+                val h = mutable.height
+                val w = mutable.width
                 paint.textSize = h/15f
                 paint.strokeWidth = h/85f
-                var x = 9
+                var x = 0
                 scores.forEachIndexed { index, fl ->
                     x = index
                     x *= 4
                     if(fl > 0.5){
-                        paint.setColor(colors[index])
+                        paint.setColor(colors.get(index))
                         paint.style = Paint.Style.STROKE
-                        canvas.drawRect(RectF(locations[x+1] *w,
-                            locations[x] *h, locations[x+3] *w, locations[x+2] *h),paint)
+                        canvas.drawRect(RectF(locations.get(x+1)*w, locations.get(x)*h, locations.get(x+3)*w, locations.get(x+2)*h), paint)
                         paint.style = Paint.Style.FILL
-                        canvas.drawText(labels.get(classes[index].toInt())+" "+fl.toString(),locations[x+1]*w,locations[x]*h,paint)
+                        canvas.drawText(labels.get(classes.get(index).toInt())+" "+fl.toString(), locations.get(x+1)*w, locations.get(x)*h, paint)
                     }
                 }
 
+                binding.imageView.setImageBitmap(mutable)
+
 
             }
-
         }
 
-    }
-    @SuppressLint("MissingPermission")
-    private fun openCamera(){
-        cameraManager.openCamera(cameraManager.cameraIdList[0],object : CameraDevice.StateCallback(){
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun open_camera(){
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object:CameraDevice.StateCallback(){
             override fun onOpened(p0: CameraDevice) {
                 cameraDevice = p0
-                val surfaceTexture = binding.textureView.surfaceTexture
-                val surface = Surface(surfaceTexture)
-                val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+
+                var surfaceTexture = binding.textureView.surfaceTexture
+                var surface = Surface(surfaceTexture)
+
+                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
                 captureRequest.addTarget(surface)
 
-                cameraDevice.createCaptureSession(listOf(surface),object:CameraCaptureSession.StateCallback(){
+                cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback(){
                     override fun onConfigured(p0: CameraCaptureSession) {
-                        p0.setRepeatingRequest(captureRequest.build(),null,null)
+                        p0.setRepeatingRequest(captureRequest.build(), null, null)
                     }
-
                     override fun onConfigureFailed(p0: CameraCaptureSession) {
-
                     }
-
-                },handler)
-
+                }, handler)
             }
 
             override fun onDisconnected(p0: CameraDevice) {
@@ -145,28 +143,23 @@ class MainActivity : AppCompatActivity() {
             override fun onError(p0: CameraDevice, p1: Int) {
 
             }
-        },handler)
+        }, handler)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        model.close()
-    }
-
-    private fun getPermission() {
-        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(arrayOf(android.Manifest.permission.CAMERA),101)
+    fun get_permission(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
         }
     }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(grantResults[0]!=PackageManager.PERMISSION_GRANTED){
-            getPermission()
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            get_permission()
         }
     }
+
 }
